f03f866442b16644af98bc67b29e6a2f899ad8ea
Ariel Ramírez <aramirez@iMac-de-Armit.local>
2017-09-03 01:31:03 -0500

    Initial Commit

---
+++ compartir datos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  compartir datos
+//
+//  Created by Ariel Ramírez on 03/09/17.
+//  Copyright © 2017 Ariel Ramírez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
