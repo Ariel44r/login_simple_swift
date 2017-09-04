24f4960b901fa5c20ea1041aa8d9a6c666322d9e
Ariel Ramírez <aramirez@iMac-de-Armit.local>
2017-09-04 15:57:08 -0500

    _change_var_to_let_for_storage_constants_values_

--- compartir datos/second_viewcontroller.swift
+++ compartir datos/second_viewcontroller.swift
@@ -42,13 +42,13 @@ class second_viewcontroller: UIViewController {
     var myString_usuario = String()
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        var send_nombre = segue.destination as! third_viewcontroller
+        let send_nombre = segue.destination as! third_viewcontroller
         send_nombre.myString_nombre_vc3 = "Nombre: " + textfield_nombre_vc2.text!
-        var send_apellido = segue.destination as! third_viewcontroller
+        let send_apellido = segue.destination as! third_viewcontroller
         send_apellido.myString_apellido_vc3 = "Apellido: " + textfield_apellido_vc2.text!
-        var send_edad = segue.destination as! third_viewcontroller
+        let send_edad = segue.destination as! third_viewcontroller
         send_edad.myString_edad_vc3 = "Edad: " + textfield_edad_vc2.text!
-        var send_genero = segue.destination as! third_viewcontroller
+        let send_genero = segue.destination as! third_viewcontroller
         send_genero.myString_genero_vc3 = "Género: " + textfield_genero_vc2.text!
     }
     
